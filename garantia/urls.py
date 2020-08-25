369c5db29b3f6c977f8ee1889087d9ce1fea99ec
Gustavo Mex <gustavoppymex@gmail.com>
2020-08-24 20:35:04 -0500

    Configuramos la app de garantias, y se colocaron sus respectivos filtros

--- garantia/urls.py
+++ garantia/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
-from .views import GarantiasList, GarantiasCompletas, GarantiasCreate, GarantiasUpdate, GarantiasDelete
+from .views import GarantiasList, GarantiasCompletas, GarantiasCreate, GarantiasUpdate, GarantiasDelete, personal_filter, fecha_filter, personal_filter_terminados, fecha_filter_terminado
+
 garantias_urls = ([
-    path('', GarantiasList.as_view(), name='index'),
+    path('pendientes/', GarantiasList.as_view(), name='index'),
     path('completas/', GarantiasCompletas.as_view(), name='completas'),
     path('create/', GarantiasCreate.as_view(), name='create'),
     path('update/<int:pk>/', GarantiasUpdate.as_view(), name='update'),
     path('delete/<int:pk>/', GarantiasDelete.as_view(), name='delete'),
+    path('pendientes/personal/', personal_filter, name='filter_personal'),
+    path('pendientes/fecha/', fecha_filter, name='filter_fecha'),
+    path('completas/personal/', personal_filter_terminados, name='filter_personal_completas'),
+    path('completas/fecha/', fecha_filter_terminado, name='filter_fecha_completas')
+
+
 ], 'garantias')
--- garantia/views.py
+++ garantia/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import GarantiaModel
+from registration.models import Profile
 from .forms import GarantiasFormCreate, GarantiasFormUpdate
 from django.http import HttpResponse, HttpResponseRedirect
+import datetime
 
 class GarantiasList(ListView):
     model = GarantiaModel
@@ -13,6 +15,10 @@ class GarantiasList(ListView):
     def get_queryset(self):
         return GarantiaModel.objects.filter(estado='pendiente')
 
+    def get_context_data(self, **kwargs):
+        context = super(GarantiasList, self).get_context_data(**kwargs)
+        context['profiles'] = Profile.objects.all()
+        return context
     
 class GarantiasCompletas(ListView):
     model = GarantiaModel
@@ -20,6 +26,11 @@ class GarantiasCompletas(ListView):
 
     def get_queryset(self):
         return GarantiaModel.objects.filter(estado='terminado')
+
+    def get_context_data(self, **kwargs):
+        context = super(GarantiasCompletas, self).get_context_data(**kwargs)
+        context['profiles'] = Profile.objects.all()
+        return context
 
 class GarantiasCreate(CreateView):
     model = GarantiaModel
@@ -31,33 +42,125 @@ class GarantiasCreate(CreateView):
 
     success_url = reverse_lazy('garantias:index')
 
-class GarantiasUpdate(UpdateView):
+class GarantiasUpdate(UpdateView): 
     model = GarantiaModel
-    form_class = GarantiasFormUpdate    
+    form_class = GarantiasFormUpdate
     template_name = 'garantia/garantias_update.html'
     context_object_name = 'obj'
-    # success_url = reverse_lazy('garantias:index')
-
-    def post(self, request, pk):
-        if request.method == 'POST':
-            estado = request.POST.get('estado', )
-            if estado == 'pendiente':
-                new_form = form.save()
-                return HttpResponseRedirect('/garantias/create')
-            else:
-                return HttpResponseRedirect('/garantias/completas')
-        else:
-            return HttpResponseRedirect('/garantias/')
-
-    # def get_context_data(self, **kwargs):
-    #     estado = request.POST.get('estado', )
-
-    #     if estado == 'pendiente':
-    #         return HttpResponseRedirect('/garantias/')
-    #     else:
-    #         return HttpResponseRedirect('/garantias/')
+    success_url = reverse_lazy('garantias:index')
 
 class GarantiasDelete(DeleteView):
     model = GarantiaModel
     template_name = 'garantia/garantias_delete.html'
     success_url = reverse_lazy('garantias:index')
+
+def personal_filter(request):
+    profiles = Profile.objects.all()
+
+    if request.method == 'POST':
+        select_value = request.POST.get('myselect', )
+        estado = request.POST.get('estado', )
+
+        if select_value == 'all':
+            return redirect('garantias:index')
+
+        else:
+            list_item_post = GarantiaModel.objects.filter(tecnico=select_value, estado='pendiente')
+            
+    else:
+        return redirect('garantias:index')
+
+    context = {
+        'profiles': profiles,
+        'list_item_post':list_item_post
+    }
+
+    return render(request, 'garantia/garantias_personal.html', context)
+
+
+def fecha_filter(request):
+    profiles = Profile.objects.all()
+
+    if request.method == 'POST':
+
+        select_value = request.POST.get('select_date', )
+
+        if select_value == 'all':
+            return redirect('garantias:index')
+
+        elif select_value == '2019':
+            print('2019')
+            list_item_post_date = GarantiaModel.objects.filter(creacion__year = '2019', estado='pendiente')
+         
+        elif select_value == '2020':
+            print('2020')
+            list_item_post_date = GarantiaModel.objects.filter(creacion__year = '2020', estado='pendiente')
+ 
+        else:
+            return redirect('garantias:index') 
+
+    else:
+        return redirect('garantias:index') 
+
+    context = {
+        'profiles': profiles,
+        'list_item_post_date':list_item_post_date
+    }
+
+    return render(request, 'garantia/garantias_fecha.html', context)
+
+
+def personal_filter_terminados(request):
+    profiles = Profile.objects.all()
+
+    if request.method == 'POST':
+        select_value = request.POST.get('myselect', )
+        estado = request.POST.get('estado', )
+
+        if select_value == 'all':
+            return redirect('garantias:completas')
+
+        else:
+            list_item_pendiente = GarantiaModel.objects.filter(tecnico=select_value, estado='terminado')
+            
+    else:
+        return redirect('garantias:index')
+
+    context = {
+        'profiles': profiles,
+        'list_item_pendiente':list_item_pendiente
+    }
+
+    return render(request, 'garantia/garantias_personal_terminado.html', context)
+
+
+def fecha_filter_terminado(request):
+    profiles = Profile.objects.all()
+
+    if request.method == 'POST':
+
+        select_value = request.POST.get('select_date', )
+
+        if select_value == 'all':
+            return redirect('garantias:completas')
+
+        elif select_value == '2019':
+            print('2019')
+            list_item_terminado = GarantiaModel.objects.filter(creacion__year = '2019', estado='terminado')
+         
+        elif select_value == '2020':
+            print('2020')
+            list_item_terminado = GarantiaModel.objects.filter(creacion__year = '2020', estado='terminado')
+ 
+        else:
+            return redirect('garantias:index') 
+
+    else:
+        return redirect('garantias:index') 
+
+    context = {
+        'profiles': profiles,
+        'list_item_terminado':list_item_terminado
+    }
+
+    return render(request, 'garantia/garantias_fecha_terminado.html', context)
