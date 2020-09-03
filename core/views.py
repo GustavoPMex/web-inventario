690ba98f4a20fa30caa1be0f3e2fd638699bef88
Gustavo Mex <gustavoppymex@gmail.com>
2020-09-03 15:56:51 -0500

    Configuración de home

--- core/views.py
+++ core/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from garantia.models import GarantiaModel
+from taller.models import EquipoModel
 
 def HomeView(request):
     garantias = GarantiaModel.objects.filter(estado='pendiente')
+    equipos = EquipoModel.objects.filter(estado='pendiente')
 
     context = {
         'garantias':garantias,
+        'equipos':equipos,
     }
 
     return render(request, 'core/home.html', context)
