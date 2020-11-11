561d0085427bdfec6d51ea982da5c035afe0d425
Gustavo Mex <gustavoppymex@gmail.com>
2020-11-11 17:55:03 -0600

    Version previa a los cambios en la app taller y servicios

--- registration/views.py
+++ registration/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth.models import User
 
 class StaffRequiredMixin(object):
     def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_superuser:
+        if request.user.is_superuser:
             return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
 
         return HttpResponseNotAllowed(['GET', 'POST'])
